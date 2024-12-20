9a2e5991855ecfdb190dbb3b9b8ecf8788424b94
clearlysid <siddharth99c@gmail.com>
2024-12-20 16:57:50 +0530

    feat: adds keypress listener

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rdev::{listen, Event, EventType};
+
+fn callback(event: Event) {
+    match event.event_type {
+        EventType::KeyPress(key) => {
+            println!("Key: {:?}", key);
+        },
+        _ => {}
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .setup(|_| {
+        if let Err(error) = listen(callback) {
+            println!("Error: {:?}", error)
+        }
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
