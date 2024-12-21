5d361340e4bec43aa35fdad48664fbac224f1f19
clearlysid <siddharth99c@gmail.com>
2024-12-21 17:17:53 +0530

    feat: adds system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
+use tauri::SystemTray;
 use rdev::{listen, EventType, set_is_main_thread};
 
 fn main() {
+
+    // Setup System Tray
+    let tray = SystemTray::new();
+
     tauri::Builder::default()
     .setup(|app| {
         let app_handle = app.handle();
@@ -29,11 +34,25 @@ fn main() {
             }) {
                 println!("Error: {:?}", error)
             }
-
         });
-
-
         Ok(())
+    })
+    .system_tray(tray)
+    .on_system_tray_event(|_app, event| match event {
+        tauri::SystemTrayEvent::LeftClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            if let Some(window) = _app.get_window("main") {
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                }
+            }
+        }
+        _ => {}
     })
     .invoke_handler(tauri::generate_handler![])
     .run(tauri::generate_context!())
